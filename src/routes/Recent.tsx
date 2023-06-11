2ddd8b927bd4589e6d7782114168b6261591d130
yoonseooo71 <yoonseooo71@gmail.com>
2023-06-11 21:44:08 +0900

    publishing recent

--- src/routes/Recent.tsx
+++ src/routes/Recent.tsx
@@ -1,9 +1,51 @@
+import { styled } from "styled-components";
+import Post from "../components/Post";
+const dummydata= {
+  title: "JavaScript 와 Java 어떤게 좋나요",
+  optionA: "JavaScript",
+  optionB: "Java", 
+  date : "2023-05-31", 
+  commant : 5,
+  totalVotes:777,
+  AVotes:444,
+  BVotes:333,
+}
 const Recent = ()=>{
   return (
-    <>
-    recent
-    </>
+    <Wrapper>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+      <Post postData={dummydata}/>
+    </Wrapper>
   )
 }
 
-export default Recent ;
+const Wrapper = styled.div`
+  width: 1400px;
+  display: flex;
+  flex-wrap: wrap;
+  @media all and (max-width: 1400px){
+    width: 1050px;
+  }
+  @media all and (max-width: 1050px){
+    width: 90%;
+  }
+  
+`
+
+export default Recent ; 
